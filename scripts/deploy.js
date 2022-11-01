9ca2a6fe8c261ad2cdafe5d4a86ee6b0acb831f3
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-01 23:06:44 +0900

    deploy to Mumbai

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,35 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const main = async () => {
+  const domainContractFactory = await hre.ethers.getContractFactory("PolygonDomains");
+  const domainContract = await domainContractFactory.deploy("ninja");
+  await domainContract.deployed();
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  console.log("Contract deployed to:", domainContract.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  let txn = await domainContract.register("banana", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await txn.wait();
+  console.log("Minted domain banana.ninja");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  txn = await domainContract.setRecord("banana", "Am I a banana or a ninja??");
+  await txn.wait();
+  console.log("Set record for banana.ninja");
 
-  await lock.deployed();
+  const address = await domainContract.getAddress("banana");
+  console.log("Owner of domain banana:", address);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
