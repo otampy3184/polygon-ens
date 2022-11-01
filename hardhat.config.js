6c575f0a80265f629487ca6dd5519686c0087617
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-01 23:10:17 +0900

    Mumbaiデプロイのための設定

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.API_KEY,
+      accounts: [process.env.SECRET_KEY],
+    },
+    matic: {
+      url: process.env.API_KEY_MUM,
+      accounts: [`0x${process.env.SECRET_KEY}`],
+    },
+  },
 };
