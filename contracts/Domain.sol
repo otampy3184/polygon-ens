43d42eaea1061931648a29c537b5f263487c23d7
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-01 22:41:28 +0900

    指定のRecordを保存できるようにする

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -7,12 +7,16 @@ contract PolygonDomains {
     // mappingでString型の各KeyとAddressを結びつける
     mapping(string => address) public domainsToAddress;
 
+    // stringとStringを紐づけるMapping
+    mapping(string => string) public records;
+
     constructor() {
         console.log("Construct Success");
     }
 
     // msg.senderのアドレスに指定の名前のDomainで登録する
     function register(string calldata name) public {
+        require(domainsToAddress[name] == address(0));
         domainsToAddress[name] = msg.sender;
         console.log("%s has regiered a domain", msg.sender);
     }
@@ -20,6 +24,15 @@ contract PolygonDomains {
     // Nameに紐づいたDomainのアドレスを返す
     function getAddress(string calldata name) public view returns (address){
         return domainsToAddress[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domainsToAddress[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
     }
 }
 
