5e675987461df08b2f4b2179601a5089d0f3d476
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-03 00:08:45 +0900

    connect wallet

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,12 @@ import { ethers } from "ethers";
 import abi from "./utils/PolygonDomains.json";
 
 function App() {
-  const [ currentAccount, setCurrentAccount] = useState("");
+  const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
-      if(!ethereum) {
+      if (!ethereum) {
         console.log("make sure you connected metamask");
         return
       } else {
@@ -18,21 +18,42 @@ function App() {
       }
 
       const accounts = await ethereum.request({ method: "eth_accounts" });
-      if (accounts.length !== 0){
+      if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("we found account", account);
         setCurrentAccount(account);
       } else {
         console.log("no authorized account")
       }
-    } catch(error){
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("get metamask");
+        return;
+      }
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected:", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
       console.log(error);
     }
   }
 
   return (
     <div className="App">
-
+      <div>
+        <button className='button' onClick={connectWallet}>connect wallet</button>
+      </div>    
     </div>
   );
 }
