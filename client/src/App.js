051928934470b5a6e3901fb7ed01b4e63b5f1486
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-02 23:59:46 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from "./utils/PolygonDomains.json";
+
 function App() {
+  const [ currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if(!ethereum) {
+        console.log("make sure you connected metamask");
+        return
+      } else {
+        console.log("Metamask detected");
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0){
+        const account = accounts[0];
+        console.log("we found account", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("no authorized account")
+      }
+    } catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
