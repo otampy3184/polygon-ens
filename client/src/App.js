46463cdf39bfe4117945c06c7745ecb902338ba3
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-03 23:23:39 +0900

    domainをMintする関数

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,62 @@ function App() {
     }
   }
 
+  const mintDomain = async () => {
+    // ドメインがnullのときrunしません。
+    if (!domain) {
+      return;
+    }
+    // ドメインが3文字に満たない、短すぎる場合にアラートを出します。
+    if (domain.length < 3) {
+      alert("Domain must be at least 3 characters long");
+      return;
+    }
+    // ドメインの文字数に応じて価格を計算します。
+    // 3 chars = 0.05 MATIC, 4 chars = 0.03 MATIC, 5 or more = 0.01 MATIC
+    const price =
+      domain.length === 3 ? "0.05" : domain.length === 4 ? "0.03" : "0.01";
+    console.log("Minting domain", domain, "with price", price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        // ミントされるまでトランザクションを待ちます。
+        const receipt = await tx.wait();
+
+        // トランザクションが問題なく実行されたか確認します。
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // domain,recordをセットします。
+          tx = await contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log("Record set! https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+          setRecord("");
+          setDomain("");
+        } else {
+          alert("Transaction failed! Please try again");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
       <img
